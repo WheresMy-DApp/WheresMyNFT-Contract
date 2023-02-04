318282ee6e50a5ab7a098f38e687b1d9ab6ebdac
Pranav M S <pranavms13@yahoo.com>
2023-02-05 02:43:39 +0530

    Modifications

--- contracts/WheresMyNFT.sol
+++ contracts/WheresMyNFT.sol
@@ -1,46 +1,53 @@
 pragma solidity ^0.8.0;
 
-contract WheresMyNFT {
+contract LocationNFT {
     uint256 public tokenId;
-    uint[] public latitudes;
-    uint[] public longitudes;
-    uint[] public timestamps;
+
+    address public authorizedUser;
     
     mapping (uint256 => bool) public tokens;
     mapping (uint256 => bytes12) public deviceMacAddresses;
+    mapping (uint256 => uint[]) public latitudesArrays;
+    mapping (uint256 => uint[]) public longitudesArrays;
+    mapping (uint256 => uint[]) public timestampsArrays;
     
     constructor () public {
         tokenId = 0;
     }
-    
+
     function createNFT(uint[] memory _latitudes, uint[] memory _longitudes, uint[] memory _timestamps, bytes12 _deviceMacAddress) public {
         tokenId++;
         tokens[tokenId] = true;
-        latitudes.push(_latitudes);
-        longitudes.push(_longitudes);
-        timestamps.push(_timestamps);
+        latitudesArrays[tokenId] = _latitudes;
+        longitudesArrays[tokenId] = _longitudes;
+        timestampsArrays[tokenId] = _timestamps;
         deviceMacAddresses[tokenId] = _deviceMacAddress;
     }
     
-    function getLatitudes(uint256 _tokenId) public view returns (uint[] memory) {
-        return latitudes[_tokenId];
+    function getLatitudes(uint256 _tokenId) public isAuthorized view returns (uint[] memory) {
+        return latitudesArrays[_tokenId];
     }
     
-    function getLongitudes(uint256 _tokenId) public view returns (uint[] memory) {
-        return longitudes[_tokenId];
+    function getLongitudes(uint256 _tokenId) public isAuthorized view returns (uint[] memory) {
+        return longitudesArrays[_tokenId];
     }
     
-    function getTimestamps(uint256 _tokenId) public view returns (uint[] memory) {
-        return timestamps[_tokenId];
+    function getTimestamps(uint256 _tokenId) public isAuthorized view returns (uint[] memory) {
+        return timestampsArrays[_tokenId];
     }
     
-    function getDeviceMacAddress(uint256 _tokenId) public view returns (bytes12) {
+    function getDeviceMacAddress(uint256 _tokenId) public isAuthorized view returns (bytes12) {
         return deviceMacAddresses[_tokenId];
     }
     
     function transfer(uint256 _tokenId, address _to) public {
         require(tokens[_tokenId] == true, "Token does not exist");
         tokens[_tokenId] = false;
-        _to.transfer(_tokenId);
+        payable(_to).transfer(_tokenId);
+    }
+
+    modifier isAuthorized() {
+        require(msg.sender == authorizedUser, "Not authorized");
+        _;
     }
 }
